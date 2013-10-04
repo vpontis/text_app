8fddbbfade839e86a9dc554de88fcbba1485af17
Victor Pontis <vpontis@gmail.com>
2013-10-04 00:23:49 -0400

    added user text relation in the models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131003160417) do
+ActiveRecord::Schema.define(version: 20131004041525) do
 
   create_table "texts", force: true do |t|
     t.text     "body"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20131003160417) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "seconds"
+    t.integer  "user_id"
   end
 
   add_index "texts", ["body"], name: "index_texts_on_body", using: :btree
